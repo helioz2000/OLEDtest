e5e061a74b5199eb7bcb9a2bc52b5e1c48021fff
helioz2000 <helioz2000@gmail.com>
2017-06-16 22:44:07 +1000

    graphic+text modes

--- OLEDtest.ino
+++ OLEDtest.ino
@@ -2,57 +2,77 @@
  * Test for OLED display
  * requires U8g2 library by oliver
  * https://github.com/olikraus/u8g2/wiki/
+ * 
+ * Connection:
+ * OLED -> Arduino Nano
+ * GND -> GND
+ * VCC -> 5V
+ * SCL -> A5
+ * SDA -> A4
  */
+
+// We can run two modes, text or graphics. 
+// Comment out the line below for graphics mode
+#define TEXTMODE
 
 #include <Arduino.h>
 #include <SPI.h>
-#include <U8x8lib.h>
-#include <U8g2lib.h>
 
+#ifdef TEXTMODE
+#include <U8x8lib.h>
 /* Constructor for text mode */
 U8X8_SSD1306_128X32_UNIVISION_HW_I2C u8x8(/* clock=*/ 21, /* data=*/ 20);
+#else
+#include <U8g2lib.h>
 /* Constructor for graphic mode */
 U8G2_SSD1306_128X32_UNIVISION_1_HW_I2C u8g2(/* rotation=*/ U8G2_R0, /* clock=*/ 21, /* data=*/ 20);
+#endif
 
 uint8_t contrast;
 bool dir;
 
 void setup(void)
 {
-  Serial.begin(9600); 
-  //u8x8.begin(); /* u8x8.begin() is required and will sent the setup/init sequence to the display */
-  u8g2.begin();
-  //u8x8.setFont(u8x8_font_chroma48medium8_r);
-  //u8x8.drawString(0,0," OLED by VK3ERW ");
-  
-  //u8x8.drawString(0,1,"1:");
-  //u8x8.setFont(u8x8_font_amstrad_cpc_extended_r);
-  //u8x8.setFont(u8x8_font_5x7_r);
-  //u8x8.setFont(u8x8_font_5x8_r);
-  //u8x8.setFont( u8x8_font_artossans8_r );
-  //u8x8.setFont( u8x8_font_victoriabold8_r );
-  //u8x8.setFont( u8x8_font_pressstart2p_r );
+  Serial.begin(9600);
+#ifdef TEXTMODE
+  u8x8.begin(); /* u8x8.begin() is required and will sent the setup/init sequence to the display */
+  u8x8.setFont(u8x8_font_chroma48medium8_r);
+  u8x8.drawString(0,0," OLED by VK3ERW ");
+  u8x8.drawString(0,1,"1: Hello World");
+  u8x8.setFont(u8x8_font_amstrad_cpc_extended_r);
+  u8x8.setFont(u8x8_font_5x7_r);
+  u8x8.setFont(u8x8_font_5x8_r);
+  u8x8.setFont( u8x8_font_artossans8_r );
+  u8x8.setFont( u8x8_font_victoriabold8_r );
+  u8x8.setFont( u8x8_font_pressstart2p_r );
   //u8x8.drawString(0,2,"2: Hello World");
   //u8x8.setInverseFont(1);
-  //u8x8.drawString(0,3,"0123456789012345");
+  u8x8.drawString(0,3,"0123456789012345");
+  u8x8.drawString(0,2,"Contrast:");
+#else
+  u8g2.begin();
+#endif
   contrast = 255;
   dir = false;
-  //u8x8.drawString(0,2,"Contrast:");
+  
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, LOW);
 
 }
 
 void loop(void)
 {
-  //u8x8.setContrast(contrast);
-  //u8x8.setCursor(10,2);
-  //u8x8.print ("   ");
-  //u8x8.setCursor(10,2);
-  //u8x8.print(contrast);
-  //Serial.print("Contrast: ");
-  //Serial.println(contrast);
-
-  //delay(2000);
+#ifdef TEXTMODE
+  u8x8.setContrast(contrast);
+  u8x8.setCursor(10,2);
+  u8x8.print ("   ");
+  u8x8.setCursor(10,2);
+  u8x8.print(contrast);
+  Serial.print("Contrast: ");
+  Serial.println(contrast);
+  delay(200);
   //u8x8.setPowerSave(1);
+#endif
   delay(80);
   if (dir == false) {
     if (contrast > 0) {
@@ -69,11 +89,16 @@ void loop(void)
       delay(2000);
     }
   }
+  
+#ifndef TEXTMODE
   u8g2.firstPage();
   do {
     u8g2.setFont(u8g2_font_ncenB14_tr);
     u8g2.drawStr(20,28,"VK3ERW");
   } while ( u8g2.nextPage() );
+#endif
 
+#ifdef TEXTMODE
   //u8x8.setPowerSave(0);
+#endif
 }
